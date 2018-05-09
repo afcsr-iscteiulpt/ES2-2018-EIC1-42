8b708a5bd6df692be44647a17e1c8e3058718df2
agpco-iscteiul <agpco@iscte-iul.pt>
2018-05-09 18:07:25 +0100

    Tweaks and improvements

--- src/General/BinaryProblem.java
+++ src/General/BinaryProblem.java
@@ -1,25 +1,50 @@
 package General;
 
 
+import java.util.BitSet;
+
 import org.uma.jmetal.problem.impl.AbstractBinaryProblem;
 import org.uma.jmetal.solution.BinarySolution;
 
 public class BinaryProblem  extends AbstractBinaryProblem {
 	private Problem problema;
 	
-	public BinaryProblem(Problem prob,int num_var) {
-		this(prob.getVariablesArray().size());
-		problema = prob;
+	public BinaryProblem(Problem problema,int num_var) {
+		this(problema.getVariablesArray().size());
+		createBinaryProblem(problema);
 	}
 	
 	public BinaryProblem(int num_Var) {
 		setNumberOfVariables(num_Var);
+
+	}
+	
+	public void createBinaryProblem(Problem problema) {
+		this.problema = problema;
 		setNumberOfObjectives(2);//ver objetivos (perguntar ao utilizador) !!conferir com a função evaluate!! default:2
 		setName(problema.getName());
 	}
 
 	@Override
-	public void evaluate(BinarySolution sol) {
+	public void evaluate(BinarySolution solution) {
+		
+	    int counterOnes;
+	    int counterZeroes;
+	    counterOnes = 0;
+	    counterZeroes = 0;
+
+	    BitSet bitset = solution.getVariableValue(0) ;
+	    for (int i = 0; i < bitset.length(); i++) {
+	      if (bitset.get(i)) {
+	        counterOnes++;
+	      } else {
+	        counterZeroes++;
+	      }
+	    }
+	    // OneZeroMax is a maximization problem: multiply by -1 to minimize
+	    solution.setObjective(0, -1.0 * counterOnes);
+	    solution.setObjective(1, -1.0 * counterZeroes);		  
+	    
 		// TODO Auto-generated method stub
 		// Ir buscar e correr o jar com uma função de evaluate aqui ...
 		//sol.setObjective(arg0, arg1);
--- src/General/IntegerProblem.java
+++ src/General/IntegerProblem.java
@@ -10,13 +10,17 @@ public class IntegerProblem extends AbstractIntegerProblem{
 	private Problem problema;
 	
 
-	public IntegerProblem(Problem prob) {
-		this(prob.getVariablesArray().size());
-		problema = prob;
+	public IntegerProblem(Problem problema) {
+		this(problema.getVariablesArray().size());
+		createIntegerProblem(problema);
 	}
 	
 	public IntegerProblem(int num_Var) {
 		setNumberOfVariables(num_Var);
+	}
+	
+	public void createIntegerProblem(Problem problema) {
+		this.problema = problema;
 		setNumberOfObjectives(/*problema.getOjetivos()*/2);//ver objetivos (perguntar ao utilizador)default:2
 		setName(problema.getName());
 		
@@ -30,11 +34,30 @@ public class IntegerProblem extends AbstractIntegerProblem{
 		
 		setLowerLimit(lowerLimit);
 		setUpperLimit(upperLimit);
-		
 	}
 	
 	@Override
-	public void evaluate(IntegerSolution Sol) {
+	public void evaluate(IntegerSolution solution) {
+		
+	    double[] fx = new double[getNumberOfObjectives()];
+	    int[] x = new int[getNumberOfVariables()];
+	    for (int i = 0; i < solution.getNumberOfVariables(); i++) {
+	      x[i] = solution.getVariableValue(i) ;
+	    }
+
+	    fx[0] = 0;
+	    for (int var = 0; var < solution.getNumberOfVariables() - 1; var++) {
+		  fx[0] += Math.abs(x[0]+Math.random()*10); // Example for testing
+	    }
+	    
+	    fx[1] = 0;
+	    for (int var = 0; var < solution.getNumberOfVariables(); var++) {
+	    	fx[1] += Math.abs(x[1]+Math.random()*10); // Example for testing
+	    }
+
+	    solution.setObjective(0, fx[0]);
+	    solution.setObjective(1, fx[1]);
+		
 		// TODO Auto-generated method stub
 		// Ir buscar e correr o jar com uma função de evaluate aqui ...
 		//sol.setObjective(arg0, arg1);

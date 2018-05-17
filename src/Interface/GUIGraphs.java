2abf1aec5c0675e6f3345519403aec7e76846464
dprsa1-iscteiulpt <dprsa1@iscte-iul.pt>
2018-05-17 12:47:48 +0100

    actualização de painel de graficos

--- src/Interface/GUIGraphs.java
+++ src/Interface/GUIGraphs.java
@@ -1,33 +1,46 @@
 package Interface;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import General.SharedClass;
+import graphs.Scale;
 
-public class GUIGraphs extends JFrame{
+import java.awt.Color;
 
-	private Scale scale = new Scale();
-	private JPanel panel;
+public class GUIGraphs extends JFrame {
+
+	private JPanel contentPane;
 	private SharedClass shared;
-	private JPanel PanelGraph = scale.getPanel() ;
-//	private Scale scale = new Scale();
+	private Scale PANELGraphs;
 
-
+	//--
 	public GUIGraphs(SharedClass shared) {
-		this.shared=shared;
-		getContentPane().setLayout(null);
-	
-		PanelGraph.setBounds(6, 6, 438, 199);
-		getContentPane().add(PanelGraph);
-	
+		this.shared = shared;
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 700, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		PANELGraphs = new Scale();
+		PANELGraphs.setBounds(6, 6, 688, 466);
+		PANELGraphs.setBackground(Color.WHITE);
+		contentPane.add(PANELGraphs);
 	}
-	
-	public JPanel getPanel(){
-		return PanelGraph;
+
+	//
+	public JPanel getContentPane(){
+		return contentPane;
 	}
+	public JPanel getGraphPanel(){
+		System.out.println("teste");
+		return PANELGraphs;
+	}
+	//--
+
+
 }
